5ed6bd710f07af072b3fb4593d78335de9e7123f
Berjak <jjhb94@gmail.com>
2019-04-20 19:21:35 -0500

    managed to remove the entire error page and insert username and password errors upon invalid input; the is_valid does not check for invalid input of the username though :/

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-from flask import Flask, request, render_template
-import cgi
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 app.config['# DEBUG'] = True
 
 form = ""
 
+    
 
-@app.route("/user", methods=['POST'])
-def hello():
 
-    email_name = ""
+@app.route("/", methods=['POST'])
+def validate():
+
+    email_name = request.form["Email-Address"]
     user_name = request.form["User-name"] #this takes the number field and converts it into int
+    username_error =''
+    password_error =''
+
     user_password = request.form["Password"] # this takes what is is the text box and passes it to the the variable 
     confirm_password = request.form["Confirm-Password"]
     
-    if user_password != confirm_password:
-        return render_template("error.html") # this redirects to the same input page BUT with the error messages instead
-    return render_template("add-confirmation.html", valid_credentials=user_name)#'<h1> Welcome, ' + user_name +'</h1>'
-    
+    if (not user_name) or (not is_valid(user_name)) or (' ' in user_name):
+        username_error = 'That is not a valid username'
+    if (not username_error):
+        if user_password != confirm_password:
+            password_error = 'that is not a valid password'
+        return render_template("add-confirmation.html", valid_credentials=user_name)#'<h1> Welcome, ' + user_name +'</h1>'
+       
+    else:
+        return render_template('index.html', valid_credentials=user_name, invalid_credentials=username_error)
+
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html") 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
